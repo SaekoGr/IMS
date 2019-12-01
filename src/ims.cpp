ff5c64f9124f45dd926ed5771309ccd396d3e28e
Sabína Gregušová <sabina.gregusova@gmail.com>
2019-12-01 18:34:25 +0100

    Started programming

---
+++ src/ims.cpp
@@ -0,0 +1,16 @@
+/**
+ * IMS Project
+ * Simulation of middle-size European town and it's daily production of CO2
+ * originating from the use of personal vehicles and buses
+ * 
+ * Sabína Gregušová (xgregu02)
+ * Filip Weigel (xweige01)
+ * 
+ */
+
+#include <stdio.h>
+
+int main(){
+    printf("Hello World!\n");
+    return 0;
+}
